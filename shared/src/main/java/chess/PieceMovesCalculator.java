78e1b738243e43b1c59b2364553515507c8fa41f
eat100 <eat100@byu.edu>
2024-05-27 16:31:18 -0600

    Phase 3 -- commit 20 (autograder annoying)

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -19,56 +19,30 @@ public class PieceMovesCalculator {
     }
 
     public ArrayList<ChessMove> horizontal() {
-        ArrayList<ChessMove> moves = new ArrayList<>();
-        int row = position.getRow();
-        int col = position.getColumn();
-        ChessPosition start = new ChessPosition(row, col);
-
-        // Directions for horizontal and vertical moves
         int[][] directions = {
-                {1, 0},   // up
-                {-1, 0},  // down
-                {0, 1},   // right
-                {0, -1}   // left
+                {1, 0},  // up
+                {-1, 0}, // down
+                {0, 1},  // right
+                {0, -1}  // left
         };
-
-        for (int[] direction : directions) {
-            int newRow = row;
-            int newCol = col;
-            while (true) {
-                newRow += direction[0];
-                newCol += direction[1];
-                if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) {
-                    break; // Out of board bounds
-                }
-                ChessPosition end = new ChessPosition(newRow, newCol);
-                if (board.hasPiece(end)) {
-                    if (board.getPiece(end).getTeamColor() != color) {
-                        moves.add(new ChessMove(start, end));
-                    }
-                    break; // Stop if there's a piece in the way
-                } else {
-                    moves.add(new ChessMove(start, end));
-                }
-            }
-        }
-
-        return moves;
+        return generateMoves(directions);
     }
 
     public ArrayList<ChessMove> diagonal() {
+        int[][] directions = {
+                {1, 1},  // up-right
+                {1, -1}, // up-left
+                {-1, 1}, // down-right
+                {-1, -1} // down-left
+        };
+        return generateMoves(directions);
+    }
+
+    private ArrayList<ChessMove> generateMoves(int[][] directions) {
         ArrayList<ChessMove> moves = new ArrayList<>();
         int row = position.getRow();
         int col = position.getColumn();
         ChessPosition start = new ChessPosition(row, col);
-
-        // Directions for diagonal moves
-        int[][] directions = {
-                {1, 1},   // up-right
-                {1, -1},  // up-left
-                {-1, 1},  // down-right
-                {-1, -1}  // down-left
-        };
 
         for (int[] direction : directions) {
             int newRow = row;
@@ -106,20 +80,7 @@ public class PieceMovesCalculator {
                 {1, 0}, {-1, 0}, {0, 1}, {0, -1}
         };
 
-        for (int[] offset : moveOffsets) {
-            int newRow = row + offset[0];
-            int newCol = col + offset[1];
-            if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
-                ChessPosition end = new ChessPosition(newRow, newCol);
-                if (board.hasPiece(end)) {
-                    if (board.getPiece(end).getTeamColor() != color) {
-                        moves.add(new ChessMove(start, end));
-                    }
-                } else {
-                    moves.add(new ChessMove(start, end));
-                }
-            }
-        }
+        addMovesFromOffsets(moves, moveOffsets, start);
 
         return moves;
     }
@@ -136,6 +97,15 @@ public class PieceMovesCalculator {
                 {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}
         };
 
+        addMovesFromOffsets(moves, moveOffsets, start);
+
+        return moves;
+    }
+
+    private void addMovesFromOffsets(ArrayList<ChessMove> moves, int[][] moveOffsets, ChessPosition start) {
+        int row = start.getRow();
+        int col = start.getColumn();
+
         for (int[] offset : moveOffsets) {
             int newRow = row + offset[0];
             int newCol = col + offset[1];
@@ -150,8 +120,6 @@ public class PieceMovesCalculator {
                 }
             }
         }
-
-        return moves;
     }
 
     public ArrayList<ChessMove> pawnMoves(ChessGame.TeamColor color) {
@@ -169,39 +137,46 @@ public class PieceMovesCalculator {
         ChessPosition atkLeft = new ChessPosition(row + direction, col - 1);
         ChessPosition atkRight = new ChessPosition(row + direction, col + 1);
 
-        // Check for promotion
-        if (end.getRow() == promotionRow) {
-            if (!board.hasPiece(end)) {
-                addPromotionMoves(moves, start, end);
-            }
-            if (board.hasPiece(atkLeft) && board.getPiece(atkLeft).getTeamColor() != color) {
-                addPromotionMoves(moves, start, atkLeft);
-            }
-            if (board.hasPiece(atkRight) && board.getPiece(atkRight).getTeamColor() != color) {
-                addPromotionMoves(moves, start, atkRight);
-            }
-        }
-        // Check for initial double advance
-        else if (start.getRow() == startRow) {
-            if (!board.hasPiece(end)) {
-                moves.add(new ChessMove(start, end));
-                if (!board.hasPiece(longEnd)) {
-                    moves.add(new ChessMove(start, longEnd));
-                }
-            }
-            addCaptureMoves(moves, start, atkLeft, color);
-            addCaptureMoves(moves, start, atkRight, color);
-        }
-        // Normal move
-        else {
-            if (!board.hasPiece(end)) {
-                moves.add(new ChessMove(start, end));
-            }
-            addCaptureMoves(moves, start, atkLeft, color);
-            addCaptureMoves(moves, start, atkRight, color);
+        if (row + direction == promotionRow) {
+            handlePromotionMoves(moves, start, end, atkLeft, atkRight);
+        } else if (row == startRow) {
+            handleInitialPawnMoves(moves, start, end, longEnd, atkLeft, atkRight);
+        } else {
+            handleNormalPawnMoves(moves, start, end, atkLeft, atkRight);
         }
 
         return moves;
+    }
+
+    private void handlePromotionMoves(ArrayList<ChessMove> moves, ChessPosition start, ChessPosition end, ChessPosition atkLeft, ChessPosition atkRight) {
+        if (!board.hasPiece(end)) {
+            addPromotionMoves(moves, start, end);
+        }
+        if (board.hasPiece(atkLeft) && board.getPiece(atkLeft).getTeamColor() != color) {
+            addPromotionMoves(moves, start, atkLeft);
+        }
+        if (board.hasPiece(atkRight) && board.getPiece(atkRight).getTeamColor() != color) {
+            addPromotionMoves(moves, start, atkRight);
+        }
+    }
+
+    private void handleInitialPawnMoves(ArrayList<ChessMove> moves, ChessPosition start, ChessPosition end, ChessPosition longEnd, ChessPosition atkLeft, ChessPosition atkRight) {
+        if (!board.hasPiece(end)) {
+            moves.add(new ChessMove(start, end));
+            if (!board.hasPiece(longEnd)) {
+                moves.add(new ChessMove(start, longEnd));
+            }
+        }
+        addCaptureMoves(moves, start, atkLeft, color);
+        addCaptureMoves(moves, start, atkRight, color);
+    }
+
+    private void handleNormalPawnMoves(ArrayList<ChessMove> moves, ChessPosition start, ChessPosition end, ChessPosition atkLeft, ChessPosition atkRight) {
+        if (!board.hasPiece(end)) {
+            moves.add(new ChessMove(start, end));
+        }
+        addCaptureMoves(moves, start, atkLeft, color);
+        addCaptureMoves(moves, start, atkRight, color);
     }
 
     private void addPromotionMoves(ArrayList<ChessMove> moves, ChessPosition start, ChessPosition end) {
