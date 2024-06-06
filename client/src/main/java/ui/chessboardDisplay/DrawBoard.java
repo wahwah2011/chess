5b959141951dc3bf7c03c0755cefe9f0929bd961
eat100 <eat100@byu.edu>
2024-06-06 16:41:15 -0600

    Phase 5 -- commit 6 (added functionality that changes piece text color based on team color)

--- client/src/main/java/ui/chessboardDisplay/DrawBoard.java
+++ client/src/main/java/ui/chessboardDisplay/DrawBoard.java
@@ -1,6 +1,7 @@
 package ui.chessboardDisplay;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
@@ -20,9 +21,14 @@ public class DrawBoard {
         DrawBoard drawBoard = new DrawBoard();
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
-        drawBoard.drawChessBoard(out);
+        drawBoard.drawChessBoard(out, "w");
 
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(EMPTY.repeat(10));
+        out.print(RESET_BG_COLOR);
+        out.print('\n');
 
+        drawBoard.drawChessBoard(out, "b");
     }
 
     public DrawBoard() {
@@ -70,25 +76,24 @@ public class DrawBoard {
 
     //add a non-default constructor that takes a DisplayBoard as input
 
-    public void drawChessBoard(PrintStream out) {
+    public void drawChessBoard(PrintStream out, String playerColor) {
         //keep track of team color; normal order if white, reverse if black
-        drawHeaders(out,"b");
-        drawRowSquares(out);
-
-        drawHeaders(out, "b");
+        drawHeaders(out,playerColor);
+        drawRowSquares(out, playerColor);
+        drawHeaders(out, playerColor);
     }
 
-    private void drawHeaders(PrintStream out, String color) {
+    private void drawHeaders(PrintStream out, String playerColor) {
         //header or footer
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_BLACK);
         out.print(SET_TEXT_BOLD);
         String[] headers = {"   ", " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", "   "};
         String[] reverseHeaders = {"   ", " h ", " g ", " f ", " e ", " d ", " c ", " b ", " a ", "   "};
-        if (color.equals("b")) {
+        if (playerColor.equals("b")) {
             printHeaderText(out,headers);
         }
-        else if (color.equals("w")) {
+        else if (playerColor.equals("w")) {
             printHeaderText(out,reverseHeaders);
         }
         out.print(RESET_TEXT_BOLD_FAINT);
@@ -102,12 +107,23 @@ public class DrawBoard {
         out.print("\n");
     }
 
-    private void drawRowSquares(PrintStream out) {
+    private void drawRowSquares(PrintStream out, String playerColor) {
         int[] rowNumbers = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] revRowNumbers = {8, 7, 6, 5, 4, 3, 2, 1};
+
+        if (playerColor.equals("w")) {
+            drawRows(out, revRowNumbers);
+        }
+        else if (playerColor.equals("b")) {
+            drawRows(out, rowNumbers);
+        }
+    }
+
+    private void drawRows(PrintStream out, int[] rowNumbers) {
         for (int row = 0; row < rowNumbers.length; row++) {
             drawRowNum(out, rowNumbers[row]);
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; boardCol++) {
-                drawSquare(out, row, boardCol);
+                drawSquare(out, rowNumbers[row], boardCol);
             }
             drawRowNum(out, rowNumbers[row]);
             out.print("\n");
@@ -124,29 +140,28 @@ public class DrawBoard {
     }
 
     private void drawSquare(PrintStream out, int row, int col) {
-        ChessPosition pos = new ChessPosition(row + 1,col + 1);
-        String piece = "   ";
+        ChessPosition pos = new ChessPosition(row,col + 1);
+
         if (isWhite(row,col)) {
             out.print(SET_BG_COLOR_WHITE);
-            out.print(SET_TEXT_COLOR_BLACK);
         }
         else  {
             out.print(SET_BG_COLOR_BLACK);
-            out.print(SET_TEXT_COLOR_WHITE);
         }
+
+        drawPiece(out, pos);
+    }
+
+    private void drawPiece(PrintStream out, ChessPosition pos) {
+        String piece = "   ";
+        String textColor;
 
         if (chessBoard.hasPiece(pos)) {
             piece = pieceToText(pos);
+            textColor = teamTextColor(pos);
+            out.print(textColor);
         }
         out.print(piece);
-    }
-
-    private void drawPlayer(PrintStream out) {
-
-    }
-
-    private ChessPiece.PieceType checkType(DisplayPiece piece) {
-        return piece.piece().getPieceType();
     }
 
     private boolean isWhite(int row, int col) {
@@ -176,6 +191,19 @@ public class DrawBoard {
             return PAWN;
         }
         else return null;
+    }
+
+    private String teamTextColor(ChessPosition pos) {
+        ChessPiece currPiece = chessBoard.getPiece(pos);
+        ChessGame.TeamColor color = currPiece.getTeamColor();
+
+        if (color.equals(ChessGame.TeamColor.WHITE)) {
+            return SET_TEXT_COLOR_BLUE;
+        }
+        else if (color.equals(ChessGame.TeamColor.BLACK)) {
+            return SET_TEXT_COLOR_RED;
+        }
+        return SET_TEXT_COLOR_MAGENTA;
     }
 
     @Override
